6a06fb26c94df0c6cac0ff92257b15c87be24a9d
Sami Kerola <kerolasa@iki.fi>
2016-08-23 19:25:40 +0100

    portability: fix float max check from values.h
    MacOS does not have values.h file.

--- mtr.c
+++ mtr.c
@@ -27,7 +27,9 @@
 #include <string.h>
 #include <strings.h>
 #include <error.h>
+#ifdef HAVE_VALUES_H
 #include <values.h>
+#endif
 
 #include <netdb.h>
 #include <netinet/in.h>
@@ -209,8 +211,11 @@ strtofloat_or_err (const char *str, const char *errmesg)
   if (str != NULL && *str != '\0') {
       errno = 0;
       num = strtod (str, &end);
-      if (errno == 0 && str != end && end != NULL && *end == '\0' &&
-          num < FLT_MAX)
+      if (errno == 0 && str != end && end != NULL && *end == '\0'
+#ifdef FLT_MAX
+         && num < FLT_MAX
+#endif
+         )
 	return num;
     }
   error (EXIT_FAILURE, errno, "%s: '%s'", errmesg, str);
