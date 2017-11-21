9b3aba317c0a8be0087eee77fe2db2e70b60584c
R.E. Wolff <R.E.Wolff@BitWizard.nl>
2017-11-21 17:20:42 +0100

    better fix. to dave's problem.

--- ui/split.c
+++ ui/split.c
@@ -166,7 +166,7 @@ int split_keyaction(
     tv.tv_usec = 0;
 
     if (select(1, &readfds, NULL, NULL, &tv) > 0) {
-        if (read(0, &c, 1) < 0) 
+        if (read(0, &c, 1) <= 0) 
           return ActionQuit;
     } else
         return 0;
